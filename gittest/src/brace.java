52ba6632132dd2642ed37aa78f58830047e23b00
Higgles <thomas.higgins.2@ucdconnect.ie>
2014-01-28 13:39:25 +0000

    projtest

---
+++ gittest/src/brace.java
@@ -0,0 +1,39 @@
+import java.util.Stack;
+
+public class brace
+{
+
+	public static void main(String[] args)
+	{
+		bn b;
+		b=new bn();
+		System.out.println(b.bracematcher("{1+[2+(3+4)]}}"));
+
+	}
+	
+	
+
+}
+
+class bn
+{
+	Boolean bracematcher(String x){
+		Stack<Character> a= new Stack<Character>();
+		String brackets="({[)}]";
+		int pos=0;
+		for(char c :x.toCharArray()){
+			pos=brackets.indexOf(c);
+			if(pos>=0&&pos<brackets.length()/2)
+				a.push(brackets.charAt(pos+(brackets.length()/2)));
+			else if(pos>=brackets.length()/2)
+				if(!a.isEmpty()&&c==a.peek())
+					a.pop();
+				else
+					return false;
+		}
+		if(a.isEmpty())
+			return true;
+		else
+			return false;
+	}
+}
